54cbf41ff9e57006eee14dfd73f0a5ca684abbd3
Terry Luciano <terryluciano1@gmail.com>
2026-02-24 10:57:53 -0500

    Updating build_docker script by adding an optional arg that will either expose the docker container locally or on the network

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
 docker build -t pomodoro-timer .
 docker rm -f pomodoro
-docker run -d --name pomodoro --restart always -p 2525:2525 pomodoro-timer
+
+if [[ $1 == "host" ]]; then
+    echo "Exposing to all interfaces"
+    docker run -d --name pomodoro --restart always -p 2505:2505 pomodoro-timer
+else 
+    echo "Exposing to localhost only"
+    docker run -d --name pomodoro --restart always -p 127.0.0.1:2505:2505 pomodoro-timer
+fi
